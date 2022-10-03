0775ee9984aa5cabe66e92c36f6b838a2c6c1fb9
Muxan <Muxan94@gmail.com>
2022-10-03 12:07:08 +0300

    Update

--- src/com/company/HomeWork01.java
+++ src/com/company/HomeWork01.java
@@ -2,6 +2,13 @@ package com.company;
 
 public class HomeWork01 {
     public static void main(String[] args) {
-        
+        System.out.println("Song - Billie Jean");
+        System.out.println("Artist - Michael Jackson");
+        System.out.println("Text:\n" +
+                "She was more like a beauty queen from a movie scene\n" +
+                "I said don't mind,\n" +
+                "But what do you mean I am the one\n" +
+                "Who'll dance on the floor in the round\n" +
+                "She said I am the one who'll dance on the floor in the round" );
     }
 }
